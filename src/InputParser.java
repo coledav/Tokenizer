a9e3ddb812b11a41ebe164933781f2871371ce79
cole.827 <cole.827@PC-DL172B.cse.ohio-state.edu>
2017-10-01 15:17:57 -0400

    Got tokenizer working for reserved words

--- src/InputParser.java
+++ src/InputParser.java
@@ -1,31 +1,71 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InputParser {
 
     private static FileReader fileReader;
     private String program;
+    private static String tokenType;
+    private static String idValue;
+    private static int intValue;
 
-    public static void main(String[] args) {
+    private static Map<String, Integer> tokenNumbers = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
         String filename = args[0];
+        loadTokenMap();
         System.out.println(filename);
 
-        try {
-            fileReader = new FileReader(filename);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        fileReader = new FileReader(filename);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        try {
-            System.out.println(bufferedReader.readLine());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Tokenizer tokenizer = new Tokenizer(bufferedReader);
 
-        //loop through the tokens and print them all out
+        //loop through all tokens, looking at their values and prints each tokens' corresponding number
+        idValue = tokenizer.getToken();
+        while (!idValue.equals("EOF")) {
+            System.out.println(tokenNumbers.get(idValue));
+            tokenizer.skipToken();
+            idValue = tokenizer.getToken();
+        }
+    }
+
+    private static void loadTokenMap() {
+        tokenNumbers.put("program", 1);
+        tokenNumbers.put("begin", 2);
+        tokenNumbers.put("end", 3);
+        tokenNumbers.put("int", 4);
+        tokenNumbers.put("if", 5);
+        tokenNumbers.put("then", 6);
+        tokenNumbers.put("else", 7);
+        tokenNumbers.put("while", 8);
+        tokenNumbers.put("loop", 9);
+        tokenNumbers.put("read", 10);
+        tokenNumbers.put("write", 11);
+        tokenNumbers.put(";", 12);
+        tokenNumbers.put(",", 13);
+        tokenNumbers.put("=", 14);
+        tokenNumbers.put("!", 15);
+        tokenNumbers.put("[", 16);
+        tokenNumbers.put("]", 17);
+        tokenNumbers.put("&&", 18);
+        tokenNumbers.put("||", 19);
+        tokenNumbers.put("(", 20);
+        tokenNumbers.put(")", 21);
+        tokenNumbers.put("+", 22);
+        tokenNumbers.put("-", 23);
+        tokenNumbers.put("*", 24);
+        tokenNumbers.put("!=", 25);
+        tokenNumbers.put("==", 26);
+        tokenNumbers.put("<", 27);
+        tokenNumbers.put(">", 28);
+        tokenNumbers.put("<=", 29);
+        tokenNumbers.put(">=", 30);
+        tokenNumbers.put("integer", 31);
+        tokenNumbers.put("id", 32);
+        tokenNumbers.put("EOF", 33);
     }
 
 }
--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -1,36 +1,314 @@
-import java.util.ArrayList;
+import java.io.BufferedReader;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Tokenizer {
 
-    private String inputStream;
-    private ArrayList<String> tokens;
-    private char curr;
-    private final List<Character> SYMBOLS = Arrays.asList(';', ',', '=', '!',
-            '[', ']', '&', '|', '(', ')', '+', '-', '*', '<', '>');
+    private static BufferedReader reader;
+    private static Queue<String> tokens;
+    private static char curr;
 
-    public Tokenizer(String input) {
-        this.inputStream = input;
-        this.tokens = new ArrayList<String>();
+    private static final List<Character> SYMBOLS = Arrays.asList(';', ',', '=',
+            '!', '[', ']', '&', '|', '(', ')', '+', '-', '*', '<', '>');
+
+    private static final List<Character> WHITE_SPACE = Arrays.asList(' ', '\n',
+            '\r', '\t');
+
+    public Tokenizer(BufferedReader reader) throws Exception {
+        Tokenizer.reader = reader;
+        Tokenizer.tokens = new LinkedList<String>();
+        tokenize();
     }
 
-    private void tokenize() {
-        for (int i = 0; i < this.inputStream.length(); i++) {
-            this.curr = this.inputStream.charAt(i);
+    private static void tokenize() throws Exception {
+        int c;
+        while ((c = reader.read()) != -1) {
+            curr = (char) c;
+            if (curr == 'p') {
+                readProgram();
+            } else if (curr == 'b') {
+                readBegin();
+            } else if (curr == 'e') {
+                curr = (char) reader.read();
+                if (curr == 'l') {
+                    readElse();
+                } else if (curr == 'n') {
+                    readEnd();
+                } else {
+                    throw new Exception("Invalid token");
+                }
+            } else if (curr == 'i') {
+                curr = (char) reader.read();
+                if (curr == 'n') {
+                    readInt();
+                } else if (curr == 'f') {
+                    readIf();
+                } else {
+                    throw new Exception("Invalid token");
+                }
+            } else if (curr == 'w') {
+                curr = (char) reader.read();
+                if (curr == 'h') {
+                    readWhile();
+                } else if (curr == 'r') {
+                    readWrite();
+                } else {
+                    throw new Exception("Invalid token");
+                }
+            } else if (curr == 't') {
+                readThen();
+            } else if (curr == 'l') {
+                readLoop();
+            } else if (curr == 'r') {
+                readRead();
+            } else if (WHITE_SPACE.contains(curr)) {
+                continue;
+            }
+        }
+        tokens.add("EOF");
+    }
 
+    private static void readProgram() throws Exception {
+        boolean valid = false;
+        int c = reader.read();
+        curr = (char) c;
+        if (curr == 'r') {
+            curr = (char) reader.read();
+            if (curr == 'o') {
+                curr = (char) reader.read();
+                if (curr == 'g') {
+                    curr = (char) reader.read();
+                    if (curr == 'r') {
+                        curr = (char) reader.read();
+                        if (curr == 'a') {
+                            curr = (char) reader.read();
+                            if (curr == 'm') {
+                                c = reader.read();
+                                if (WHITE_SPACE.contains((char) c) || c == -1) {
+                                    tokens.add("program");
+                                    valid = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!valid) {
+            throw new Exception("String not valid");
         }
     }
 
-    //Returns "id" if current token is an ID and returns "int" if token is an int
-    public String getToken() {
-        return "";
+    private static void readBegin() throws Exception {
+        boolean valid = false;
+        int c = reader.read();
+        curr = (char) c;
+        if (curr == 'e') {
+            curr = (char) reader.read();
+            if (curr == 'g') {
+                curr = (char) reader.read();
+                if (curr == 'i') {
+                    curr = (char) reader.read();
+                    if (curr == 'n') {
+                        c = reader.read();
+                        if (WHITE_SPACE.contains((char) c) || c == -1) {
+                            tokens.add("begin");
+                            valid = true;
+                        }
+                    }
+                }
+            }
+        }
+        if (!valid) {
+            throw new Exception("String not valid");
+        }
+    }
 
+    private static void readEnd() throws Exception {
+        boolean valid = false;
+        int c = reader.read();
+        curr = (char) c;
+        if (curr == 'd') {
+            c = reader.read();
+            if (WHITE_SPACE.contains((char) c) || c == -1) {
+                tokens.add("end");
+                valid = true;
+            }
+        }
+        if (!valid) {
+            throw new Exception("String not valid");
+        }
+    }
+
+    private static void readElse() throws Exception {
+        boolean valid = false;
+        int c = reader.read();
+        curr = (char) c;
+        if (curr == 's') {
+            curr = (char) reader.read();
+            if (curr == 'e') {
+                c = reader.read();
+                if (WHITE_SPACE.contains((char) c) || c == -1) {
+                    tokens.add("else");
+                    valid = true;
+                }
+            }
+        }
+        if (!valid) {
+            throw new Exception("String not valid");
+        }
+    }
+
+    private static void readInt() throws Exception {
+        boolean valid = false;
+        int c = reader.read();
+        curr = (char) c;
+        if (curr == 't') {
+            c = reader.read();
+            if (WHITE_SPACE.contains((char) c) || c == -1) {
+                tokens.add("int");
+                valid = true;
+            }
+        }
+        if (!valid) {
+            throw new Exception("String not valid");
+        }
+    }
+
+    private static void readIf() throws Exception {
+        boolean valid = false;
+        int c = reader.read();
+        curr = (char) c;
+        if (WHITE_SPACE.contains(curr) || c == -1) {
+            tokens.add("if");
+            valid = true;
+        }
+        if (!valid) {
+            throw new Exception("String not valid");
+        }
+    }
+
+    private static void readThen() throws Exception {
+        boolean valid = false;
+        int c = reader.read();
+        curr = (char) c;
+        if (curr == 'h') {
+            curr = (char) reader.read();
+            if (curr == 'e') {
+                curr = (char) reader.read();
+                if (curr == 'n') {
+                    c = reader.read();
+                    if (WHITE_SPACE.contains((char) c) || c == -1) {
+                        tokens.add("then");
+                        valid = true;
+                    }
+                }
+            }
+        }
+        if (!valid) {
+            throw new Exception("String not valid");
+        }
+    }
+
+    private static void readWhile() throws Exception {
+        boolean valid = false;
+        int c = reader.read();
+        curr = (char) c;
+        if (curr == 'i') {
+            curr = (char) reader.read();
+            if (curr == 'l') {
+                curr = (char) reader.read();
+                if (curr == 'e') {
+                    c = reader.read();
+                    if (WHITE_SPACE.contains((char) c) || c == -1) {
+                        tokens.add("while");
+                        valid = true;
+                    }
+                }
+            }
+        }
+        if (!valid) {
+            throw new Exception("String not valid");
+        }
+    }
+
+    private static void readWrite() throws Exception {
+        boolean valid = false;
+        int c = reader.read();
+        curr = (char) c;
+        if (curr == 'i') {
+            curr = (char) reader.read();
+            if (curr == 't') {
+                curr = (char) reader.read();
+                if (curr == 'e') {
+                    c = reader.read();
+                    if (WHITE_SPACE.contains((char) c) || c == -1) {
+                        tokens.add("write");
+                        valid = true;
+                    }
+                }
+            }
+        }
+        if (!valid) {
+            throw new Exception("String not valid");
+        }
+    }
+
+    private static void readLoop() throws Exception {
+        boolean valid = false;
+        int c = reader.read();
+        curr = (char) c;
+        if (curr == 'o') {
+            curr = (char) reader.read();
+            if (curr == 'o') {
+                curr = (char) reader.read();
+                if (curr == 'p') {
+                    c = reader.read();
+                    if (WHITE_SPACE.contains((char) c) || c == -1) {
+                        tokens.add("loop");
+                        valid = true;
+                    }
+                }
+            }
+        }
+        if (!valid) {
+            throw new Exception("String not valid");
+        }
+    }
+
+    private static void readRead() throws Exception {
+        boolean valid = false;
+        int c = reader.read();
+        curr = (char) c;
+        if (curr == 'e') {
+            curr = (char) reader.read();
+            if (curr == 'a') {
+                curr = (char) reader.read();
+                if (curr == 'd') {
+                    c = reader.read();
+                    if (WHITE_SPACE.contains((char) c) || c == -1) {
+                        tokens.add("read");
+                        valid = true;
+                    }
+                }
+            }
+        }
+        if (!valid) {
+            throw new Exception("String not valid");
+        }
+    }
+
+    //Returns string value of current token
+    public String getToken() {
+        return tokens.peek();
     }
 
     //skips current token
     public void skipToken() {
-
+        tokens.remove();
     }
 
     //returns value of int token
