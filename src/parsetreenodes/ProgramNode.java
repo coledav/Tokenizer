3bb8b79028c499a583b2dc2295b3e787110aa6b9
cole.827 <cole.827@osu.edu>
2017-10-16 19:08:48 -0400

    Made skeleton ProgramNode class

---
+++ src/parsetreenodes/ProgramNode.java
@@ -0,0 +1,24 @@
+package parsetreenodes;
+
+public class ProgramNode {
+	private DeclSeqNode ds;
+	private SSNode ss;
+	
+	
+	public ProgramNode(){
+		this.ds = new DeclSeqNode();
+		this.ss = new SSNode();
+	}
+	
+	public void parseProgram(){
+		
+	}
+	
+	public void printProgram(){
+		
+	}
+	
+	public void execProgram(){
+		
+	}
+}
