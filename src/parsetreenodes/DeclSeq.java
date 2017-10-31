06242c87d247126b3f5381834eb4283b1f788754
cole.827 <cole.827@osu.edu>
2017-10-31 08:54:27 -0400

    DeclSeq and StatementSeq

---
+++ src/parsetreenodes/DeclSeq.java
@@ -0,0 +1,26 @@
+package parsetreenodes;
+
+import com.sun.javafx.css.Declaration;
+
+public class DeclSeq {
+    private Declaration decl;
+    private DeclSeq ds;
+
+    public DeclSeq() {
+        this.decl = null;
+        this.ds = null;
+    }
+
+    public void parseDeclSeq() {
+
+    }
+
+    public void printDeclSeq() {
+
+    }
+
+    public void execDeclSeq() {
+
+    }
+
+}
---
+++ src/parsetreenodes/StatementSeq.java
@@ -0,0 +1,23 @@
+package parsetreenodes;
+
+public class StatementSeq {
+    private Statement statement;
+    private StatementSeq ss;
+
+    public DeclSeq() {
+        this.statement = null;
+        this.ss = null;
+    }
+
+    public void parseStatementSeq() {
+
+    }
+
+    public void printStatementSeq() {
+
+    }
+
+    public void execStatementSeq() {
+
+    }
+}
